8eabbaed0d3d9c39c7856f3b0652172870e39e92
Rasande <andreas@rasande.se>
2020-04-09 09:23:14 +0200

    Create navbar-brand.php

--- inc/classes.php
+++ inc/classes.php
@@ -13,7 +13,10 @@ defined( 'ABSPATH' ) || exit;
 if ( ! function_exists( 'rasande_change_logo_class' ) ) {
 	// Replaces logo CSS class.
 	function rasande_change_logo_class( $html ) {
+		$html = str_replace( 'class="custom-logo"', 'class="img-fluid" id="rotate--scroll"', $html );
+		$html = str_replace( 'class="custom-logo-link"', 'class="navbar-brand custom-logo-link"', $html );
 		$html = str_replace( 'alt=""', 'title="Home" alt="logo"', $html );
+
 		return $html;
     }
     add_filter( 'get_custom_logo', 'rasande_change_logo_class' );
