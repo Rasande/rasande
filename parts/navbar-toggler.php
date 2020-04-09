8a13871a48b907345d4b08d9ac1f2c24b2f22348
Rasande <andreas@rasande.se>
2020-04-09 09:22:22 +0200

    Add hamburgers

---
+++ parts/navbar-toggler.php
@@ -0,0 +1,16 @@
+<?php 
+/**
+ * Navbar toggle part.
+ * 
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+?>
+
+<button class="navbar-toggler hamburger hamburger--squeeze" type="button" data-toggle="collapse" data-target="#navbar--main" aria-controls="navbar--main" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'rasande' ); ?>">
+    <span class="hamburger-box">
+        <span class="hamburger-inner"></span>
+    </span>	
+</button>
