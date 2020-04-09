39652c64ac0d8b0710fd579d20deee0976a13f9e
Rasande <andreas@rasande.se>
2020-04-09 09:23:44 +0200

    Add primary menu

--- header.php
+++ header.php
@@ -15,6 +15,8 @@
 			<meta http-equiv="X-UA-Compatible" content="IE=9;IE=10;IE=Edge,chrome=1"/>
 			<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
 			<link rel="profile" href="http://gmpg.org/xfn/11">
+			<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap" rel="stylesheet">
+			<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@600;700;800&display=swap" rel="stylesheet">
 			<?php wp_head(); ?>
 		</head>
 
@@ -22,9 +24,19 @@
 		<?php do_action( 'wp_body_open' ); ?>
 
 			<header class="site-header">
-				<div class="navbar navbar-expand navbar-light">
-				<div class="navbar-brand">
-				rasande
-				</div>
-				</div>
+				<nav class="navbar navbar-expand-md navbar-light fixed-top">
+						<?php get_template_part( 'parts/navbar', 'brand' ); ?>
+						<?php get_template_part( 'parts/navbar', 'toggler' ); ?>
+					<div class="collapse navbar-collapse" id="navbar--main">
+					<?php wp_nav_menu( array( 
+						'theme_location' => 'primary', 
+						'container' => '',
+						'menu_class' => 'navbar-nav ml-auto',
+						'depth' => 0,
+						'walker' => new Rasande_WP_Navwalker(),
+						 ) 
+					); ?>
+					</div>
+
+				</nav>
 			</header>
