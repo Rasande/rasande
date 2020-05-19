bd17a422908e82c850441e085947f74a638ddf72
Rasande <andreas@rasande.se>
2020-05-19 11:18:00 +0200

    Update CF7 textarea

--- src/js/modules/custom.js
+++ src/js/modules/custom.js
@@ -44,11 +44,11 @@ var autoExpand = function (field) {
 	// Calculate the height
 	var height = parseInt(computed.getPropertyValue('border-top-width'), 10)
 	             + parseInt(computed.getPropertyValue('padding-top'), 10)
-	             + field.scrollHeight
+               + field.scrollHeight
 	             + parseInt(computed.getPropertyValue('padding-bottom'), 10)
 	             + parseInt(computed.getPropertyValue('border-bottom-width'), 10);
 
-	field.style.height = height + 'px';
+	field.style.height = height - 14 + 'px';
 
 };
 
