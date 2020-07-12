05b488408c57c853314690f44d3ea06259d6509e
Rasande <andreas@rasande.se>
2020-07-12 15:26:12 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -9,14 +9,26 @@
  ?>
 
 		<footer class="site-footer">
-
+			<!-- Footer Call to Action -->
 			<div class="footer-cta">
+
+				<?php
+		
+					if( have_rows('footer_content', 'option') ):
+						while ( have_rows('footer_content', 'option') ) : the_row();
+						
+							$leftCol = get_sub_field('left_col', 'option');
+							$rightCol = get_sub_field('right_col', 'option');
+							
+						endwhile;
+						echo '</ul>';
+					endif;
+				?>
+
 				<div class="container">
 					<div class="row">
 						<div class="footer-cta-left col-xs-12 col-md-6 pt-2">
-							<p class="text-center">placeholder</p>
-							<p class="text-center"><a href="#" class="footer-cta-link">placeholder</a></p>
-							<p class="text-center"><a href="#" class="footer-cta-link">placeholder</a></p>
+							<?php echo $leftCol; ?>
 						</div>
 						<div class="footer-cta-right col-xs-12 col-md-6 pt-2">
 							<p class="text-center">placeholder</p>
@@ -25,9 +37,11 @@
 					</div>
 				</div>
 			</div>
+			<!-- Footer Information -->
 			<div class="footer-info">
 				<div class="container">
 					<div class="row">
+						<!-- Footer Menu -->
 						<div class="col-xs-12 col-md-6 text-center text-left-md pt-2">
 							<?php wp_nav_menu( array(
 								'theme_location'  => 'footer',
@@ -38,16 +52,18 @@
 								'depth'			  => '1'
 							)); ?>
 						</div>
+						<!-- Social Media -->
 						<div class="col-xs-12 col-md-6 text-center text-right-md pt-2">
 							<?php get_template_part( 'parts/social', 'media' );?>
 						</div>
+						<!-- Copyright Information -->
 						<div class="col-xs-12 text-center pt-2 color-gray-500">
 							<?php rasande_copyright(); ?>
 						</div>
 					</div>
 				</div>
 			</div>
-		</footer> <!-- end .site-footer -->
+		</footer>
 		
 		<?php wp_footer(); ?>
 		
