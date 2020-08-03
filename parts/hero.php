7bb748ba8dc9f86bf7947c96177c118e54126490
Rasande <andreas@rasande.se>
2020-08-03 12:03:16 +0200

    Update hero colors

--- parts/hero.php
+++ parts/hero.php
@@ -9,18 +9,21 @@ defined( 'ABSPATH' ) || exit;
 ?>
 
 <div class="hero">
+ <div class="hero-content">
     <div class="row align-center">
 
-        <div class="col-xs-12 col-md-6">
-            <h1 class="page-title">Andreas Jansson</h1>
+        <div class="col-xs-12 col-md-8">
+            <h1 class="page-title"><span class="primary-color">Andreas Jansson</span> utvecklare & designer</h1>
             <div class="lead">
                 <p>Andreas Jansson, frilansande utvecklare och designer i Uppsala.</p>
             </div>
         </div>
 
-        <div class="col-xs-12 col-md-6">
-            <img src="http://localhost/rasandese/wp-content/uploads/2020/04/ommiginspopic-500x466.jpg" alt="">
+        <div class="col-xs-12 col-md-4 text-right">
+            <img class="hero-img" src="http://localhost/rasandese/wp-content/uploads/2020/04/ommiginspopic-500x466.jpg" alt="">
         </div>
 
     </div>
+</div>
+<div class="hero-bg"></div>
 </div>
